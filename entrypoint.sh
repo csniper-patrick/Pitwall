116cde59dcf1c0838c42a16cf39645a1da2db1d4
Patrick Pun <patrickwypun04@gmail.com>
2026-08-17 00:23:20 +0100

    feat: add tor proxy support to entrypoint and container image

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,13 @@ cd $(dirname ${0})
 [[ -f package_install_done ]] || (
 	python -m pip install -r requirements.txt
 	apt-get -y update
-	apt-get -y install ffmpeg
+	apt-get -y install ffmpeg tor
 )
+
+if [[ "$1" == "--tor" ]]; then
+	shift
+	exec tor --SocksPort 0.0.0.0:${SOCKS_PORT:-9050} "$@"
+fi
+
 sleep ${STARTUP_DELAY:-0}
-python ${1:?Missing program}
+python ${1:?Missing program} "${@:2}"
